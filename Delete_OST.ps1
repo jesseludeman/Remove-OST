d0ee3f29141cd24550dee9f67864bc1f9b2d2e6b
Jesse Ludeman <jesse.ludeman@gmail.com>
2018-09-20 17:10:18 +1000

    Add files via upload

---
+++ Delete_OST.ps1
@@ -0,0 +1,15 @@
+﻿function Delete-OST()
+{
+
+
+    # Get a list of computers
+    $ComputerList = Get-Content C:\PowerShell\ComputerList.txt
+
+    # List the OST files
+    foreach ($computer in $ComputerList)
+    {
+        Get-ChildItem "\\$computer\c$\users\*\appdata\local\microsoft\outlook" -Include *.ost -Recurse | Remove-Item -Force -WhatIf # Remove the -WhatIf to confirm deletion
+    }
+
+
+}
