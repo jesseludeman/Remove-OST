7d04dccf14156f0d4cb8193bf2087644b1f208af
Jesse Ludeman <jesse.ludeman@gmail.com>
2018-10-04 10:40:52 +1000

    Add files via upload

---
+++ Remove-OST.ps1
@@ -0,0 +1,25 @@
+﻿function Remove-OST
+{
+    [cmdletbinding()]
+    param (
+    
+        [Parameter(ValueFromPipeline)]
+        $ComputerName
+    )
+    process 
+    { 
+        if ((Test-Connection -ComputerName $ComputerName -Count 1 -Quiet) -and (Test-Path "\\$ComputerName\c$"))
+        {
+            # Advise admin what computer I'm working on
+            Write-Host -ForegroundColor Yellow "$ComputerName is online - looking for Outlook profiles"
+
+            # Locate the files and remove them
+            Get-ChildItem "\\$ComputerName\c$\Users\*\AppData\Local\Microsoft\Outlook" -Include *.ost -Recurse | foreach { "Removing file $($_.FullName)"; Remove-Item $_ -Force }
+        }
+        else
+        {
+            Write-Host -ForegroundColor Red "$ComputerName is offline"
+        }
+
+    }
+}
